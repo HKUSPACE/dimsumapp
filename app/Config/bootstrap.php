ad215b534e9e02a71eaefb5aedeede2d617d8290
Kwun Yeung <kwun@creativeworks.com.hk>
2014-12-20 01:24:19 +0800

    load Upload plugin in bootstrap

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -71,6 +71,7 @@ Cache::config('default', array('engine' => 'File'));
  */
 
 CakePlugin::load('DebugKit');
+CakePlugin::load('Plugin');
 
 /**
  * You can attach event listeners to the request lifecycle as Dispatcher Filter. By default CakePHP bundles two filters:
